f8e4a7fd15a6c20ba19e0357b452e1e52ab50668
pitaz <peter.odekwo@gmail.com>
2019-02-06 12:24:10 +0100

    [feature #163022428] fix travis build fail

--- src/reducers/menuReducer.spec.js
+++ src/reducers/menuReducer.spec.js
@@ -4,102 +4,18 @@ import menuReducer from './menuReducer';
 
 describe('select_reducer', () => {
   it('renders with initial state', () => {
-    const action = { type: types.SET_CURRENT_USER };
-    const initialState = { isAuthenticated: false, user: undefined };
-    expect(auth({}, action)).toEqual(initialState);
+    const action = { type: types.GET_MEALS };
+    const initialState = { menu: undefined, singleMenu: null };
+    expect(menuReducer(undefined, action)).toEqual(initialState);
   });
 
   it('renders correctly', () => {
-    const action = { type: types.SET_CURRENT_USER };
-    expect(auth(undefined, action)).toMatchSnapshot();
+    const action = { type: types.GET_MEALS };
+    expect(menuReducer(undefined, action)).toMatchSnapshot();
   });
 
   it('returns the correct state', () => {
-    const action = { type: types.SET_CURRENT_USER, user: {} };
-    expect(auth(undefined, action)).toMatchSnapshot();
-  });
-
-  it('should set user if user is succesfully stored', () => {
-    const initialState = {
-      isAuthenticated: false,
-      user: {},
-      error: {},
-    };
-
-    const state = auth(initialState, {
-      type: types.SET_CURRENT_USER,
-      isAuthenticated: true,
-      user: {
-        userId: 1,
-        email: 'email',
-        username: 'wale',
-        image: 'https://hardwaremassive.com/s…cture.jpg',
-        roleId: 1,
-      },
-      error: {},
-    });
-    expect(state).toEqual({
-      isAuthenticated: true,
-      user: {
-        userId: 1,
-        email: 'email',
-        username: 'wale',
-        image: 'https://hardwaremassive.com/s…cture.jpg',
-        roleId: 1,
-      },
-      error: {},
-    });
-  });
-
-  it('renders correctly', () => {
-    const action = { type: types.SET_CURRENT_USER_FAIL };
-    expect(auth(undefined, action)).toMatchSnapshot();
-  });
-
-  it('returns the correct state', () => {
-    const action = { type: types.SET_CURRENT_USER, error: {} };
-    expect(auth(undefined, action)).toMatchSnapshot();
-  });
-
-  it('should set user if user login was unsuccessful', () => {
-    const initialState = {
-      isAuthenticated: false,
-      user: {},
-      error: {},
-    };
-
-    const state = auth(initialState, {
-      type: types.GET_MEALS,
-      menu,
-    });
-    expect(state).toEqual({
-      menu: ''
-    });
-  });
-
-  it('should return errors if login was unsuccessful', () => {
-    const initialState = {
-      isAuthenticated: false,
-      user: {},
-      error: {
-        message: 'login unsuccessful'
-      },
-    };
-
-    const state = auth(initialState, {
-      type: types.SET_CURRENT_USER_FAIL,
-      isAuthenticated: false,
-      user: {},
-      error: { message: 'Login unsuccessful' },
-    });
-    expect(state).toEqual({
-      isAuthenticated: false,
-      user: {},
-      error: { message: 'Login unsuccessful' },
-    });
-  });
-  it('returns the correct state', () => {
-    const action = { type: types.LOG_OUT_USER };
-    expect(auth(undefined, action)).toMatchSnapshot();
+    const action = { type: types.GET_MEALS, menu: [] };
+    expect(menuReducer(undefined, action)).toMatchSnapshot();
   });
 });
